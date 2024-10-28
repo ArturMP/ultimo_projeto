94dc4534258f96bea0dd0b4f2fc8ba90b193748c
ArturMP <114751420+ArturMP@users.noreply.github.com>
2024-10-28 09:07:12 -0300

    Update redesFavoritasMundo.js

--- graficos/redesFavoritasMundo.js
+++ graficos/redesFavoritasMundo.js
@@ -42,10 +42,11 @@ async function redesFavoritasMundo() {
   };
 
   // Inserir o gráfico na página
-  const grafico = document.createElement("div");
-  grafico.className = "grafico";
-  document.getElementById("graficos-container").appendChild(grafico);
-  Plotly.newPlot(grafico, data, layout);
+import { getCSS, criarGrafico } from "./common.js"
+
+// Trecho de código suprimido
+
+criarGrafico(data, layout)
 }
 
 redesFavoritasMundo();
