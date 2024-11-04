e88d8e5d74dd1cbbf0bfd27e582ca90987cffca3
ArturMP <114751420+ArturMP@users.noreply.github.com>
2024-11-04 09:23:48 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -51,7 +51,17 @@ async function quantidadeUsuariosPorRede() {
 
     }
 
-    criarGrafico(data, layout)
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config)
+}
+
 }
 
 quantidadeUsuariosPorRede()
