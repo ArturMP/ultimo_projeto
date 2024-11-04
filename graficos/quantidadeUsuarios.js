8a5250440fe0a05c0991fd4b2f7e1c299c8b28c8
ArturMP <114751420+ArturMP@users.noreply.github.com>
2024-11-04 09:14:09 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,58 +1,50 @@
-import { getCSS, tickConfig } from "./common.js"
+import { getCSS } from "./common.js";
 
-async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeDeUsuarios = Object.values(dados)
+async function redesFavoritasMundo() {
+  const url =
+    "https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json";
+  const res = await fetch(url);
+  const dados = await res.json();
+  const redes = Object.keys(dados);
+  const valores = Object.values(dados);
 
-    const data = [
-        {
-            x: nomeDasRedes, 
-            y: quantidadeDeUsuarios, 
-            type: 'bar',
-            marker: {
-                color: getCSS('--primary-color')
-            }
-        }
-    ]
+  // Criar o gráfico de pizza
+  const data = [
+    {
+      values: valores,
+      labels: redes,
+      type: "pie",
+      textinfo: "label+percent",
+    },
+  ];
 
-    const laytout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        title: {
-            text: 'Redes sociais com mais usuários',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                size: 30,
-                font: getCSS('--font')
-            }
-        },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Nome das redes',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Bilhões de usuários ativos',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        }
-    }
+  // Configurar o layout do gráfico
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, laytout)
+  const layout = {
+    plot_bgcolor: getCSS("--bg-color"),
+    paper_bgcolor: getCSS("--bg-color"),
+    title: {
+      text: "Redes sociais que os usuários mais gostam",
+      x: 0,
+      font: {
+        color: getCSS("--primary-color"),
+        family: getCSS("--font"),
+        size: 30,
+      },
+    },
+    legend: {
+      font: {
+        color: getCSS("--primary-color"),
+        size: 16,
+      },
+    },
+  };
 
-quantidadeUsuariosPorRede()
+  // Inserir o gráfico na página
+  const grafico = document.createElement("div");
+  grafico.className = "grafico";
+  document.getElementById("graficos-container").appendChild(grafico);
+  Plotly.newPlot(grafico, data, layout);
+}
+
+redesFavoritasMundo();
